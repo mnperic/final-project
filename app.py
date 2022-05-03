d50ccf217a374294f754f01c96858780ab22b7c1
Erikajhoshi <erika.hoshino92@gmail.com>
2022-05-03 14:36:32 +1000

    Upload files

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from flask import Flask, render_template
+import pickle
 
 app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
 
 @app.route('/')
-def home():
+def index():
+    return render_template('index1.html')
+
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    #int_features = [int(x) for x in request.form.values()]
+    #final_features = [np.array(int_features)]
+    #prediction = model.predict(final_features)
+    #output = np.round(prediction[0], 2)
+
+    return render_template('index.html')
+
+#@app.route('/results',methods=['POST'])
+#def results():
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+@app.route('/visualisation')
+def visualisation():
     return render_template('index.html')
 
 @app.route('/fueltype')
